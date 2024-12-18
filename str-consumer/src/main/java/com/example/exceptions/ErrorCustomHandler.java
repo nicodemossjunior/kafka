1e42b3675001edb863617fad91a58ffba9afbdd5
nicodemossjunior <nicodemossjunior@gmail.com>
2024-12-18 17:46:45 -0300

    Error handler

---
+++ str-consumer/src/main/java/com/example/exceptions/ErrorCustomHandler.java
@@ -0,0 +1,20 @@
+package com.example.exceptions;
+
+import org.springframework.kafka.listener.KafkaListenerErrorHandler;
+import org.springframework.kafka.listener.ListenerExecutionFailedException;
+import org.springframework.messaging.Message;
+import org.springframework.stereotype.Component;
+
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+@Component
+public class ErrorCustomHandler implements KafkaListenerErrorHandler {
+
+    @Override
+    public Object handleError(Message<?> message, ListenerExecutionFailedException exception) {
+        log.info("EXCEPTIN_HANDLER ::: Capturei um erro");
+        return null;
+    }
+
+}
--- str-consumer/src/main/java/com/example/listeners/StrConsumerListener.java
+++ str-consumer/src/main/java/com/example/listeners/StrConsumerListener.java
@@ -1,20 +1,22 @@
 package com.example.listeners;
 
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.annotation.TopicPartition;
 import org.springframework.stereotype.Component;
 
 import com.example.custom.StrConsumerCustomListener;
 
+import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
 
 @Log4j2
 @Component
 public class StrConsumerListener {
     
+   @SneakyThrows
    @StrConsumerCustomListener(groupId = "group-1")
    public void create(String message) {
-      log.info("CREATE ::: Receive message {}", message); 
+      log.info("CREATE ::: Receive message {}", message);
+      throw new IllegalArgumentException("EXCEPTION!");
    }
       
    @StrConsumerCustomListener(groupId = "group-1")
