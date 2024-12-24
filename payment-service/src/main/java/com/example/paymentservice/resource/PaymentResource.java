09a49259661f736bab66ac87188e9803675ce886
nicodemossjunior <nicodemossjunior@gmail.com>
2024-12-24 11:42:07 -0300

    Start project payment service

---
+++ payment-service/src/main/java/com/example/paymentservice/resource/PaymentResource.java
@@ -0,0 +1,13 @@
+package com.example.paymentservice.recource;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.example.paymentservice.model.Payment;
+
+public interface PaymentResource {
+
+    @PostMapping
+    ResponseEntity<Payment> payment(@RequestBody Payment payment);
+}
